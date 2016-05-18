c0c74140874301ff59d809964d3cd38b8cd46155
Raphaël Rochet <raphael@rri.fr>
2016-05-18 14:23:18 +0200

    Settings : use a list to add/remove streamers

--- prefs.js
+++ prefs.js
@@ -7,6 +7,7 @@ const Gettext = imports.gettext;
 const Gio = imports.gi.Gio;
 const Gtk = imports.gi.Gtk;
 const Lang = imports.lang;
+const GObject = imports.gi.GObject;
 
 let schemaDir = Extension.dir.get_child('schemas').get_path();
 let schemaSource = Gio.SettingsSchemaSource.new_from_directory(schemaDir, Gio.SettingsSchemaSource.get_default(), false);
@@ -24,20 +25,70 @@ const App = new Lang.Class(
 
     _init: function()
     {
-
-      // Prepare static controls
+      // Build widgets, bind simple fields to settings and connect buttons clicked signals
       let buildable = new Gtk.Builder();
       buildable.add_from_file( Extension.dir.get_path() + '/prefs.xml' );
-
+      this.main = buildable.get_object('prefs-widget');
       Schema.bind('interval', buildable.get_object('field_interval'), 'value', Gio.SettingsBindFlags.DEFAULT);
-      Schema.bind('streamers', buildable.get_object('field_streamers'), 'text', Gio.SettingsBindFlags.DEFAULT);
       Schema.bind('opencmd', buildable.get_object('field_opencmd'), 'text', Gio.SettingsBindFlags.DEFAULT);
       Schema.bind('hidestreamers', buildable.get_object('field_hidestreamers'), 'active', Gio.SettingsBindFlags.DEFAULT);
+      buildable.get_object('add_streamer').connect('clicked', Lang.bind(this, this._addStreamer));
+      buildable.get_object('del_streamer').connect('clicked', Lang.bind(this, this._delStreamer));
 
-      this.main = buildable.get_object('prefs-widget');
+      // Name some widgets for future reference
+      this.newStreamerEntry = buildable.get_object('field_addstreamer');
+      this.streamersList = buildable.get_object('field_streamerslist');
+
+      // Create the list's store and columns
+      this.store = new Gtk.ListStore();
+      this.store.set_column_types([GObject.TYPE_STRING]);
+      this.streamersList.model = this.store;
+
+      // let's create a simple text column, that renders as ... text !
+      let nameCol = new Gtk.TreeViewColumn( { expand: true, sort_column_id: 0, title: _("Streamer name") });
+      let nameColRenderer = new Gtk.CellRendererText;
+      nameCol.pack_start(nameColRenderer, true);
+      nameCol.add_attribute(nameColRenderer, "text", 0);
+      this.streamersList.append_column(nameCol);
+
+      // populate the list
+      this._refreshStreamersList();
+
       this.main.show_all();
       return this.main;
-    }
+    },
+
+    _addStreamer: function() {
+      let name = this.newStreamerEntry.text;
+      if (!name) return;
+      this.streamers.push(name);
+      this.newStreamerEntry.text = "";
+      Schema.set_string('streamers', this.streamers.join(','));
+      this._refreshStreamersList();
+    },
+
+    _delStreamer: function() {
+      let [selection, model, iter] = this.streamersList.get_selection().get_selected();
+      if (selection) {
+          let name = this.store.get_value(iter, 0);
+          this.store.remove(iter);
+          let index = this.streamers.indexOf(name);
+          if (index < 0) return;
+          this.streamers.splice(index, 1);
+          Schema.set_string('streamers', this.streamers.join(','));
+      }
+    },
+
+    _refreshStreamersList: function() {
+      this.streamers = Schema.get_string('streamers').split(',').sort();
+      this.store.clear();
+      for (let i = 0; i < this.streamers.length; i++) {
+            let name = this.streamers[i].trim();
+            if (!name) continue;
+            let iter = this.store.append();
+            this.store.set(iter, [0], [name]);
+        }
+    },
 
 });
 
