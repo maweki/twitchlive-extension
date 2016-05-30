5bdeae0b29cbefd269a792bfa1cd49be41c1af09
Mario Wenzel <maweki@gmail.com>
2016-05-30 21:56:34 +0200

    Added api stubs

---
+++ api.js
@@ -0,0 +1,19 @@
+const Soup = imports.gi.Soup;
+
+function load_json_async(url, fun) {
+
+}
+
+// https://github.com/justintv/Twitch-API/blob/master/v3_resources/channels.md#get-channelschannel
+// should maybe return a promise
+// should trigger icon download
+function channel(session, streamer) {
+
+}
+
+// https://github.com/justintv/Twitch-API/blob/master/v3_resources/streams.md#get-streamschannel
+// should maybe return a promise
+// should trigger icon download
+function stream(session, streamer) {
+
+}
