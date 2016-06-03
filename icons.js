ffc3ddda9c7371fd0ec3b163f027b511ce0c86b4
Mario Wenzel <maweki@gmail.com>
2016-06-03 12:03:08 +0200

    Most of the planned icon enhancements

--- icons.js
+++ icons.js
@@ -11,14 +11,27 @@ const Api = Extension.imports.api;
 
 const icons_path = GLib.get_user_cache_dir() + '/twitchlive-extension';
 
+/* exported init_icons, trigger_download_by_name, trigger_download_by_url, get_streamericon */
+
 function init_icons() {
   // everything needs to be disabled if the creation fails or if it isn't a writable directory
   GLib.mkdir_with_parents(icons_path, 448);
   Gtk.IconTheme.get_default().append_search_path(icons_path);
 }
 
+function mogrify_available() { // this result can surely be cached
+  let res = GLib.spawn_command_line_sync('mogrify -version');
+  return res[0];
+}
+
+function get_filename(streamername) {
+  let filename = icons_path + '/twitchlive-' + streamername + '.png';
+  return filename;
+}
+
 function has_icon(streamername) {
-  // check whether an icon is already available
+  // check whether any icon is already available
+  return GLib.file_test(get_filename(streamername), GLib.FileTest.EXISTS);
 }
 
 function trigger_download_by_name(streamername) {
@@ -31,36 +44,35 @@ function trigger_download_by_name(streamername) {
 }
 
 function trigger_download_by_url(streamername, imageurl) {
-  // basically what trigger_download does now but should return immediately
-  trigger_download(streamername, imageurl);
-}
-
-/*
-  New download/cache mechanism: We check against the actual filename twitch tells us and check whether this icon is available
-  if it is, we don't do anything. This name is both available from trigger_download_by_name (via api) and trigger_download_by_url.
-  alternatively, a zero-length file of the same name withouth extension could be saved.
-
-  If it is not available, download the file, post-process it (to png), if neccessary and symlink it to the name the icons recognize.
-*/
-
-function trigger_download(streamername, imageurl) {
-  // some cache clearing mechanism needs to be added
-  let extension = imageurl.split('.').pop();
-  let filename = icons_path + '/twitchlive-' + streamername + '.' + extension;
-
-  if (!GLib.file_test(filename, GLib.FileTest.EXISTS)) {
-    // this might be done with soup but works fine this way
-    let cmd = "curl -s %url% -o %output%".replace('%url%', imageurl).replace('%output%', filename);
-    GLib.spawn_command_line_sync(cmd); // download should probably be async
-
-    if (extension != 'png') { // we should actually check for the png-file
-      cmd = "mogrify -format png " + filename;
-      GLib.spawn_command_line_async(cmd);
-    }
+  // this should return immediately
+  let download_filename = imageurl.split('/').pop();
+  let unique_path = icons_path + '/' + download_filename;
+  let source_extension = imageurl.split('.').pop();
+  let target_name = icons_path + '/twitchlive-' + streamername + '.' + source_extension;
+  if (GLib.file_test(unique_path, GLib.FileTest.EXISTS)) {
+    return; // these icon names appear to be unique. We don't need to download it again.
   }
+
+  if (source_extension !== 'png' && !mogrify_available()) {
+    return; // we can't convert the file anyways so we don't even download it
+  }
+
+  // download the file
+  let cmd = "curl -s %url% -o %output%".replace('%url%', imageurl).replace('%output%', unique_path);
+  GLib.spawn_command_line_sync(cmd);
+
+  // symlink the dowloaded file to the proper name (overwrite if existing)
+  cmd = "ln -sf %linksource% %linktarget%".replace('%linksource%', unique_path).replace('%linktarget%', target_name);
+  GLib.spawn_command_line_sync(cmd);
+
+  if (source_extension !== 'png') {
+    // convert the linked file (this overwrites previously converted files)
+    cmd = "mogrify -format png " + target_name;
+    GLib.spawn_command_line_async(cmd);
+  }
+
 }
 
 function get_streamericon(streamername, style_class) {
-  // a nicer fallback - maybe some default twitch icon - needs to be added
   return new St.Icon({ icon_name: 'twitchlive-' + streamername, style_class: style_class, fallback_icon_name: 'twitchlive' });
 }
